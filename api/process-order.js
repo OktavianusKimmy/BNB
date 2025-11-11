1095185930d2404b3aa25b52e1dc73acfb6a1c86
OktavianusKimmy <kimmyyonata24@gmail.com>
2025-11-11 08:48:21 +0700

    Fixing column 6

--- api/process-order.js
+++ api/process-order.js
@@ -66,11 +66,11 @@ module.exports = async (req, res) => {
             // Menulis baris baru ke Sheet
             await sheets.spreadsheets.values.append({
                 spreadsheetId: process.env.SHEET_ID,
-                range: 'Sheet1!A:I', // Pastikan nama sheet Anda benar "Sheet1"
+                range: 'Sheet1!A:H', // Pastikan nama sheet Anda benar "Sheet1"
                 valueInputOption: 'USER_ENTERED',
                 requestBody: {
                     values: [[
-                        today, orderId, name, classInfo, phone, items, finalTotal, 'Perlu Verifikasi', false
+                        today, orderId, name, classInfo, phone, items, finalTotal, 'Perlu Verifikasi'
                     ]]
                 }
             });
